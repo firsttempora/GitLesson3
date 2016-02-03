5ff4b24a07da090bfdc9c6d1f26c12ee804e3587
Josh Laughner <joshlaugh5@yahoo.com>
2016-02-03 14:25:47 -0800

    Bugs fixed but back to windows paths.

--- SomeCode.m
+++ SomeCode.m
@@ -1,17 +1,17 @@
-homedir = '/Users/Josh';
-savepath = fullfile(homedir,'Documents','Saves',datestr(today,'yyyymmdd'));
-loadpath = fullfile(homedir,'Documents','RawData','BlackMesa-Exp42');
+homedir = 'C:\Users\Josh';
+savepath = fullfile(homedir,'My Documents','Saves',datestr(today,'yyyymmdd'));
+loadpath = fullfile(homedir,'My Documents','RawData','BlackMesa-Exp42');
 
-remotedir = '/Volumes/share-sat/';
+remotedir = 'Z:\';
 no2profiles = fullfile(remotedir,'SAT','BEHR','Monthly_NO2_Profiles');
 globepath = fullfile(remotedir,'SAT','BEHR','GLOBE_Database');
 
 F = dir(fullfile(loadpath,'*.mat'));
 for a=1:numel(F)
-    D=load(F(a).name);
+    D=load(fullfile(loadpath,F(a).name));
     for p=1:numel(D.Pixels)
-        lon = D.Pixels.Longitude;
-        lat = D.Pixels.Latitude;
-        D.Pixels.Terpres = get_globe_terpres(globepath, lon, lat);
+        lon = D.Pixels(p).Longitude;
+        lat = D.Pixels(p).Latitude;
+        D.Pixels(p).Terpres = get_globe_terpres(globepath, lon, lat);
     end
 end
